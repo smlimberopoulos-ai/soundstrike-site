8c205e96adf87f1bda6dbe20463b1d2db126b41d
smlimberopoulos-ai <smlimberopoulos@gmail.com>
2025-08-20 07:51:29 -0400

    summary
    summary

--- script.js
+++
@@ -1,9 +0,0 @@
-function playSound(type) {
-  let audio = new Audio();
-  if (type === 'portfolio') {
-    audio.src = 'assets/sounds/portfolio.mp3';
-  } else if (type === 'voice') {
-    audio.src = 'assets/sounds/voice.mp3';
-  }
-  audio.play();
-}
